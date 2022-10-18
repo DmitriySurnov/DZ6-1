18c80091a2343c6fdc7d17e333750c67896aa230
DmitriySurnov <79646813832@yandex.ru>
2022-10-19 01:03:12 +0300

    Задача решена

---
+++ DZ6-1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace DZ6_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopLevelMenu = new System.Windows.Forms.TextBox();
+            this.SubItem = new System.Windows.Forms.TextBox();
+            this.btnTopLevelMenu = new System.Windows.Forms.Button();
+            this.btnSubItem = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TopLevelMenu
+            // 
+            this.TopLevelMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TopLevelMenu.Location = new System.Drawing.Point(15, 156);
+            this.TopLevelMenu.Margin = new System.Windows.Forms.Padding(6);
+            this.TopLevelMenu.Name = "TopLevelMenu";
+            this.TopLevelMenu.Size = new System.Drawing.Size(430, 55);
+            this.TopLevelMenu.TabIndex = 2;
+            this.TopLevelMenu.Click += new System.EventHandler(this.Level_Click);
+            // 
+            // SubItem
+            // 
+            this.SubItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SubItem.Location = new System.Drawing.Point(15, 300);
+            this.SubItem.Margin = new System.Windows.Forms.Padding(6);
+            this.SubItem.Name = "SubItem";
+            this.SubItem.Size = new System.Drawing.Size(430, 55);
+            this.SubItem.TabIndex = 3;
+            this.SubItem.Click += new System.EventHandler(this.Level_Click);
+            // 
+            // btnTopLevelMenu
+            // 
+            this.btnTopLevelMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnTopLevelMenu.Location = new System.Drawing.Point(461, 156);
+            this.btnTopLevelMenu.Margin = new System.Windows.Forms.Padding(6);
+            this.btnTopLevelMenu.Name = "btnTopLevelMenu";
+            this.btnTopLevelMenu.Size = new System.Drawing.Size(588, 60);
+            this.btnTopLevelMenu.TabIndex = 4;
+            this.btnTopLevelMenu.Text = "Добавить пункт меню";
+            this.btnTopLevelMenu.UseVisualStyleBackColor = true;
+            this.btnTopLevelMenu.Click += new System.EventHandler(this.BtnTopLevelMenu_Click);
+            // 
+            // btnSubItem
+            // 
+            this.btnSubItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSubItem.Location = new System.Drawing.Point(461, 300);
+            this.btnSubItem.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSubItem.Name = "btnSubItem";
+            this.btnSubItem.Size = new System.Drawing.Size(588, 60);
+            this.btnSubItem.TabIndex = 5;
+            this.btnSubItem.Text = "Добавить подменю";
+            this.btnSubItem.UseVisualStyleBackColor = true;
+            this.btnSubItem.Click += new System.EventHandler(this.BtnSubItem_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.Location = new System.Drawing.Point(157, 64);
+            this.label.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(565, 48);
+            this.label.TabIndex = 7;
+            this.label.Text = "Добавления пунктов меню";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 442);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.btnSubItem);
+            this.Controls.Add(this.btnTopLevelMenu);
+            this.Controls.Add(this.SubItem);
+            this.Controls.Add(this.TopLevelMenu);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Добавление пункта меню";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TopLevelMenu;
+        private System.Windows.Forms.TextBox SubItem;
+        private System.Windows.Forms.Button btnTopLevelMenu;
+        private System.Windows.Forms.Button btnSubItem;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ DZ6-1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DZ6_1
+{
+    public partial class Form1 : Form
+    {
+        new readonly MenuStrip Menu;
+        public Form1()
+        {
+            InitializeComponent();
+            Menu = new MenuStrip();
+            MainMenuStrip = Menu;
+            Controls.Add(Menu);
+        }
+
+        private void BtnTopLevelMenu_Click(object sender, EventArgs e)
+        {
+            if (TopLevelMenu.Text == "")
+            {
+                TopLevelMenu.BackColor = Color.Red;
+                MessageBox.Show("Укажите название пункта меню", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Menu.Items.Add(TopLevelMenu.Text);
+                TopLevelMenu.BackColor = Color.Green;
+                MessageBox.Show($"{TopLevelMenu.Text} - добавлен в меню", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void BtnSubItem_Click(object sender, EventArgs e)
+        {
+            if (Menu.Items.Count < 1)
+                MessageBox.Show("У вас нет ни одного пункта меню", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else if (TopLevelMenu.Text == "")
+            {
+                TopLevelMenu.BackColor = Color.Red;
+                MessageBox.Show("Укажите название пункта меню", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (SubItem.Text == "")
+            {
+                SubItem.BackColor = Color.Red;
+                MessageBox.Show("Укажите название пункта подменю", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                for (int i = 0; i < Menu.Items.Count; i++)
+                    if (Menu.Items[i].Text == TopLevelMenu.Text)
+                    {
+                        ((ToolStripMenuItem)Menu.Items[i]).DropDownItems.Add(SubItem.Text);
+                        SubItem.BackColor = Color.Green;
+                        MessageBox.Show($"{SubItem.Text} - добавлен в меню - {TopLevelMenu.Text}", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+                TopLevelMenu.BackColor = Color.Red;
+                MessageBox.Show($"{TopLevelMenu.Text} - нет такого меню", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Level_Click(object sender, EventArgs e)
+        {
+            ((TextBox)sender).BackColor = Color.White;
+        }
+    }
+}
